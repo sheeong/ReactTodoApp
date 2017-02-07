0926312c6e4176bbb5d0c56aae96c5902f047c60
Shee Ong <sj3.ong@gmail.com>
2017-02-07 22:35:19 +0000

    create AddTodo component

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -2,12 +2,15 @@ var React = require('react');
 var Todo = require('Todo');
 
 var TodoList = React.createClass({
+  onAddTodo: function(todo) {
+    this.props.onAddTodo(todo);
+  },
   render: function() {
     var {todos} = this.props;
     var renderTodos = () => {
       return todos.map( (todo) => {
         return (
-          <Todo key={todo.id} {...todo}/>
+          <Todo key={todo.id} {...todo} onAddTodo={this.onAddTodo}/>
         );
       });
     };
