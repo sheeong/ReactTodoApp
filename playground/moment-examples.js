89edc2ffb3330ea9c0bc1d8e4e5cca88a40836f0
Shee Ong <sj3.ong@gmail.com>
2017-02-08 12:45:48 +0000

    add completed and created timestamp

---
+++ playground/moment-examples.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+
+console.log(moment().format());
+
+// January 1st 1970 @ 12.00am -> 0
+// January 1st 1970 @ 12.01am -> 60
+
+var now = moment();
+console.log('Current timestamp', now.unix());
+
+var timestamp = 1486555461;
+var currentMoment = moment.unix(timestamp);
+console.log('current moment', currentMoment.format('MMM D, YYYY @ h:mm a'));
+console.log('current moment', currentMoment.format('MMMM Do, YYYY @ h:mm A'));
